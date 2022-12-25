00c747858eafb93d8d0effd2e65a75d65bd412dd
yuvraj <yuvarma07@gmail.com>
2022-12-25 13:56:14 +0530

    Did few changes

--- src/Facebook.java
+++ src/Facebook.java
@@ -13,11 +13,11 @@ public class Facebook {
 		WebDriver driver = new ChromeDriver();  //invoke chrome browser
 		driver.get("https://facebook.com/"); //open facebook page
 		driver.findElement(By.name("email")).sendKeys("example@gmail.com"); //locate username field and insert the username
-		driver.findElement(By.name("pass")).sendKeys("example"); //locate password field and insert the password
-		driver.findElement(By.name("login")).click(); // locate and click login button
+		driver.findElement(By.xpath("//input[@name='pass']")).sendKeys("example"); //locate password field and insert the password
+		driver.findElement(By.xpath("//button[@name='login']")).click(); // locate and click login button
 		//driver.findElement(By.xpath("//button[@type=\"submit\"] | //button[@value=\"OK\"]")).click();
 		
-		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[3]/div/div[2]/div/div/div/div[1]/div/div[1]/span")).sendKeys("Hi");
+		//driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[3]/div/div[2]/div/div/div/div[1]/div/div[1]/span")).sendKeys("Hi");
 		
 		
 	
--- src/Wikipedia.java
+++ src/Wikipedia.java
@@ -3,9 +3,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class Wikipedia {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		
@@ -15,8 +17,10 @@ public class Wikipedia {
 		driver.get("https://google.com");
 		
 		driver.findElement(By.name("q")).sendKeys("Dog");
+
+		TimeUnit.SECONDS.sleep(5);
 		
-		driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]")).click();
+		driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]/div/ul/li[1]/div/div[2]")).click();
 		
 		driver.findElement(By.linkText("Wikipedia")).click();
 		}
